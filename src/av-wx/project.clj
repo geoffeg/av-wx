3df01c580e47b5442213715e04c5e542c0f83d39
Geoffrey Gallaway <geoffeg@geoffeg.org>
2013-07-26 12:36:05 +0300

    Basic json responses working.

--- src/av-wx/project.clj
+++ src/av-wx/project.clj
@@ -5,4 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.novemberain/monger "1.6.0"]
-                 [http-kit "2.1.5"]])
+                 [http-kit "2.1.8"]
+                 [compojure "1.1.5"]
+                 [ring/ring-devel "1.1.8"]
+                 [ring/ring-core "1.1.8"]
+                 [cheshire "5.2.0"]
+                 [ring/ring-json "0.2.0"]]
+  :main av-wx.core)
--- src/av-wx/src/av_wx/core.clj
+++ src/av-wx/src/av_wx/core.clj
@@ -1,10 +1,29 @@
 (ns av-wx.core
-  (:use [compojure.rotue :only [files not-found]]
+  (:use [compojure.route :only [files not-found]]
         [compojure.handler :only [site]]
         [compojure.core :only [defroutes GET POST ANY context]]
-        org.httpkit.server))
+        [org.httpkit.server]
+        [ring.middleware.json :only [wrap-json-response]]
+        [ring.util.response :only [response]])
+  (:require [ring.middleware.reload :as reload]
+            [cheshire.core :refer :all]))
+
+(defn show-index-page [args] "Meow!")
+
+(defn get-metar [search] (str "Testing!" search))
+
+(defn get-taf [search] (response {:search search}))
+
+(defn in-dev? [args] true)
 
 (defroutes all-routes
-  (GET "/" [] show-index-page))
+  (GET "/" [] show-index-page)
+  (GET "/metar/:search" [search] (get-metar search))
+  (GET "/taf/:search" [search] (get-taf search))
+  (not-found "Fer oh fer, page not fernd!"))
 
-(run-server (site #'all-routes) {:port 8080})
+(defn -main [& args] ;; entry point, lein run will pick up and start from here
+  (let [handler (if (in-dev? args)
+                  (reload/wrap-reload (site #'all-routes)) ;; only reload when dev
+                  (site all-routes))]
+    (run-server (wrap-json-response handler) {:port 8080})))
