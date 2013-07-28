ce9cd3d9f8ce21e51efd9c3c72a589547e69d6f9
Geoffrey Gallaway <geoffeg@geoffeg.org>
2013-07-28 23:50:25 +0300

    Parsing responses with clojure-csv.

--- src/av-wx/project.clj
+++ src/av-wx/project.clj
@@ -3,12 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.novemberain/monger "1.6.0"]
                  [http-kit "2.1.8"]
                  [compojure "1.1.5"]
                  [ring/ring-devel "1.1.8"]
                  [ring/ring-core "1.1.8"]
                  [cheshire "5.2.0"]
-                 [ring/ring-json "0.2.0"]]
+                 [ring/ring-json "0.2.0"]
+                 [clojure-csv/clojure-csv "2.0.1"]]
   :main av-wx.core)
--- src/av-wx/src/av_wx/reports.clj
+++ src/av-wx/src/av_wx/reports.clj
@@ -1,18 +1,19 @@
 (ns av-wx.reports
-  (:require [org.httpkit.client :as http]))
+  (:require [org.httpkit.client :as http])
+  (:use [clojure-csv.core :only [parse-csv]]
+        [clojure.pprint]))
 
-(def metar-url "http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=csv&hoursBeforeNow=4&mostRecentForEachStation=true&stationString=")
+;(def metar-url "http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=csv&hoursBeforeNow=4&mostRecentForEachStation=true&stationString=")
+(def metar-url "http://geoffeg.org/mt.txt")
 
 (defn parse-metar [csvdata]
-  (let [rdr (clojure.java.io/reader csvdata)]
-  (doseq [line (line-seq rdr)]
-    (println line))))
+  (parse-csv (subs csvdata (.indexOf csvdata "raw_text"))))
 
 (defn get-metars [search]
-  (let [{:keys [error status headers body]} @(http/get (str metar-url "KSTL"))]
+  (let [{:keys [error status headers body]} @(http/get (str metar-url) {:as :text})]
   (if error
     (println "ERROR!")
-    (println "SUCCESS" (parse-metar body)))))
+    (pprint (parse-metar body)))))
 
 (get-metars "KSFO")
 
