da8a2dc79d04066806114be0c19348f943686130
Geoffrey Gallaway <geoffeg@geoffeg.org>
2013-07-30 09:49:58 +0300

    Add bearing calculation to utils.

--- src/av-wx/src/av_wx/utils.clj
+++ src/av-wx/src/av_wx/utils.clj
@@ -19,4 +19,18 @@
            (math-f/cos long2)
            (math-f/cos long1))))))))
 
+(defn bearing-to [point1 point2]
+  (let [[lat1 long1] (deg2rad point1)
+        [lat2 long2] (deg2rad point2)
+        degrees (Math/toDegrees
+     (math-f/atan2
+       (* (math-f/sin (- long2 long1)) (math-f/cos lat2))
+       (- (* (math-f/cos lat1) (math-f/sin lat2))
+          (*
+           (math-f/sin lat1)
+           (math-f/cos lat2)
+           (math-f/cos (- long2 long1))))))]
+    (if (< degrees 0) (+ degrees 360) degrees)))
+
 (distance-between [49.2000 -98.1000] [35.9939, -78.8989])
+(bearing-to [49.2000 -98.1000] [35.9939, -78.8989])
