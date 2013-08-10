8ee20e6990ea5466b71e0763ee521f3f3318fc9d
Geoffrey Gallaway <geoffeg@geoffeg.org>
2013-08-10 18:31:47 +0300

    utils shouldn't typecast, the unput to the utils should

--- src/av-wx/src/av_wx/utils.clj
+++ src/av-wx/src/av_wx/utils.clj
@@ -10,17 +10,16 @@
 
 (defn distance-between [point1 point2]
  (let [R 6372.8 ; kilometers
-       dlat (Math/toRadians (- (Float. (point2 0)) (Float. (point1 0))))
-       dlon (Math/toRadians (- (Float. (point2 1)) (Float. (point1 1))))
-       lat1 (Math/toRadians (Float. (point1 0)))
-       lat2 (Math/toRadians (Float. (point2 0)))
+       dlat (Math/toRadians (- (point2 0) (point1 0)))
+       dlon (Math/toRadians (- (point2 1) (point1 1)))
+       lat1 (Math/toRadians (point1 0))
+       lat2 (Math/toRadians (point2 0))
        a (+ (* (Math/sin (/ dlat 2)) (Math/sin (/ dlat 2))) (* (Math/sin (/ dlon 2)) (Math/sin (/ dlon 2)) (Math/cos lat1) (Math/cos lat2)))]
    (* R 2 (Math/asin (Math/sqrt a)))))
 
 (defn bearing-to [point1 point2]
-
-  (let [[lat1 long1] (deg2rad (mapv #(Float. %) point1))
-        [lat2 long2] (deg2rad (mapv #(Float. %) point2))
+  (let [[lat1 long1] (deg2rad point1)
+        [lat2 long2] (deg2rad point2)
         degrees (Math/toDegrees
      (math-f/atan2
        (* (math-f/sin (- long2 long1)) (math-f/cos lat2))
@@ -31,6 +30,9 @@
            (math-f/cos (- long2 long1))))))]
     (if (< degrees 0) (+ degrees 360) degrees)))
 
+(defn string-to-stream [string]
+  (java.io.ByteArrayInputStream.
+   (.getBytes (.trim string))))
 
 ;; KJFK to KSFO
 ;(km2nm (distance-between [40.6397511, -73.7789256] [37.6191050, -122.3752372]))
