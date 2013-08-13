351a1bca9d830eaa67aa3194f7bbce53f3cbc77f
Geoffrey Gallaway <geoffeg@geoffeg.org>
2013-08-13 19:29:19 +0300

    Cast the CSV fields to their proper types.

--- src/av-wx/src/av_wx/reports.clj
+++ src/av-wx/src/av_wx/reports.clj
@@ -1,17 +1,48 @@
- (ns av-wx.reports
+(ns av-wx.reports
   (:require [org.httpkit.client :as http]
             [cheshire.core :refer :all]
-            [av-wx.utils :as utils]
-            [clojure.tools.trace :refer :all])
+            [av-wx.utils :as utils])
   (:use [clojure-csv.core :only [parse-csv]]
         [clojure.pprint]))
 
 (def metar-url "http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=csv&hoursBeforeNow=4&mostRecentForEachStation=true&stationString=")
-;(def metar-url "http://geoffeg.org/mt.txt")
+(def csv-field-types {"maxT24hr_c" #(Float/parseFloat %)
+                      "pcp6hr_in" #(Float/parseFloat %)
+                      "snow_in" #(Float/parseFloat %)
+                      "wind_gust_kt" #(Integer/parseInt %)
+                      "sea_level_pressure_mb" #(Float/parseFloat %)
+                      "pcp3hr_in" #(Float/parseFloat %)
+                      "minT_c" #(Float/parseFloat %)
+                      "altim_in_hg" #(Float/parseFloat %)
+                      "three_hr_pressure_tendency_mb" #(String %)
+                      "three_hr_pressure_tendancy_mb" #(Float/parseFloat %)
+                      "elevation_m" #(Float/parseFloat %)
+                      "latitude" #(Float/parseFloat %)
+                      "longitude" #(Float/parseFloat %)
+                      "minT24hr_c" #(Float/parseFloat %)
+                      "maxT_c" #(Float/parseFloat %)
+                      "visibility_statute_mi" #(Float/parseFloat %)
+                      "temp_c" #(Float/parseFloat %)
+                      "wind_speed_kt" #(Integer/parseInt %)
+                      "cloud_base_ft_agl" #(Integer/parseInt %)
+                      "vert_vis_ft" #(Integer/parseInt %)
+                      "pcp24hr_in" #(Float/parseFloat %)
+                      "precip_in" #(Float/parseFloat %)
+                      "dewpoint_in" #(Float/parseFloat %)
+                      "wind_dir_degrees" #(Integer/parseInt %)})
+
+(defn cast-csv-field [csvmap]
+  (reduce-kv
+   (fn [acc k v]
+     (update-in acc [k] #(if-not (clojure.string/blank? %) (v %))))
+   csvmap csv-field-types))
+
+(defn cast-csv-fields [csvdata]
+  (mapv cast-csv-field csvdata))
 
 (defn parse-metar [csvdata]
   (let [csvrows (parse-csv (subs csvdata (.indexOf csvdata "raw_text")))]
-  (map #(zipmap (first csvrows) %) (rest csvrows))))
+  (cast-csv-fields (map #(zipmap (first csvrows) %) (rest csvrows)))))
 
 (defn get-geoip-data [ipaddr]
   (let [{:keys [error status headers body]} @(http/get (str "http://freegeoip.net/json" ipaddr) {:as :text})]
@@ -21,15 +52,16 @@
 
 (defn append-geo-data [reports src-coords]
   (mapv #(let [report-coords [(% "latitude") (% "longitude")]]
-          (merge %
-            {"distance" (utils/distance-between src-coords report-coords),
-            "bearing" (utils/bearing-to src-coords report-coords)})) reports))
+          (assoc %
+            "distance" (utils/distance-between src-coords report-coords),
+            "bearing" (utils/bearing-to src-coords report-coords)))
+        reports))
 
 (defn get-metars [search]
   (let [{:keys [error status headers body]} @(http/get (str metar-url search) {:as :text})]
   (if error
     (println "ERROR!" error)
-    (append-geo-data (parse-metar body) (mapv (get-geoip-data "108.73.45.165") ["latitude", "longitude"])))))
+     (parse-metar body))))
 
-;(println (get-metars "KSFO"))
-;(get-geoip-data "108.73.45.165")
+;(pprint (get-metars "KSFO"))
+;(pprint (get-geoip-data "108.73.45.165"))
