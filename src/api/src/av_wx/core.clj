747c72217cf12b762fbd2eb8a9fa1e316e6b8df2
Geoffrey Gallaway <geoffeg@geoffeg.org>
2013-09-17 13:06:36 -0500

    Preliminary functioning /search with zipcode and IP addr

--- src/api/src/av_wx/core.clj
+++ src/api/src/av_wx/core.clj
@@ -10,6 +10,7 @@
             [av-wx.reports :as reports]
             [cheshire.core :refer :all]
             [av-wx.utils :as utils]
+            [av-wx.db :as db]
             [taoensso.timbre :as timbre :refer (trace debug info warn error fatal spy with-log-level)]
             [taoensso.timbre.profiling :as profiling :refer (p profile sampling-profile)]))
 
@@ -33,6 +34,7 @@
 
 (defn get-metar [search qparams remote-addr]
   (let [httploc (select-keys qparams [:latitude :longitude])]
+    (println search)
     (sampling-profile :info 0.10 :get-metar
                       (response
                        (build-response
@@ -45,13 +47,30 @@
                        (build-response
                         (p :get-taf (reports/get-tafs search)) remote-addr httploc)))))
 
-(defn in-dev? [args] true)
+(defn get-search-results [qparams remote-addr]
+    (let [searchtypes {:zipcode #(db/find-stations-zipcode % "metar")
+                       :ip      #(db/find-stations-ip % "metar")}
+          [k f] (first (select-keys searchtypes (keys qparams)))]
+      (f (qparams k))))
+
+(defn search-metar [qparams remote-addr]
+  ; three search methods are currently supported:
+  ;   zipcode=XXXXX      return airports within proximity to zipcode
+  ;   geo=lat,lon        return airports within proximity of coords
+  ;   ip=XXX.XXX.XXX.XXX airports within geolocation of IP address
+  ;   ip=@detect         airports within geolocation of client IP
+    (let [search-results (get-search-results qparams remote-addr)]
+      (if (= remote-addr "@detect") (update-in qparams "ip" remote-addr))
+      (reports/get-metars (clojure.string/join "," (map #(get % "icao") search-results)))))
+
+(defn in-dev? [args] (get-in utils/conf [:dev]))
 
 (defroutes all-routes
   (GET "/" [] show-index-page)
+  (GET "/search" {qparams :params remote-addr :remote-addr} (search-metar qparams remote-addr))
   (GET "/metar/:search" [search :as {qparams :params remote-addr :remote-addr}] (get-metar search qparams remote-addr))
   (GET "/taf/:search"  [search :as {qparams :params remote-addr :remote-addr}] (get-taf search qparams remote-addr))
-  ;(GET "/_loging/:level" [level] (timbre/set-level! level))
+  ;(GET "/_logging/:level" [level] (timbre/set-level! level))
   (not-found "Fer oh fer, page not fernd!"))
 
 (defn -main [& args] ;; entry point, lein run will pick up and start from here
--- src/api/src/av_wx/db.clj
+++ src/api/src/av_wx/db.clj
@@ -1,6 +1,7 @@
 (ns av-wx.db
   (:require [monger.core :as mg]
             [av-wx.utils :as utils]
+            [av-wx.reports :as reports]
             [monger.collection :as mc])
   (:import [com.mongodb MongoOptions ServerAddress])
   (:use [clojure.pprint]))
@@ -23,10 +24,18 @@
                          :spherical true))
             ["results"])))
 
-(defn find-zipcode [zipcode]
-  ((mc/find-one-as-map "zipcodes" {:_id (str zipcode)} {:_id 0, :loc 1}) :loc))
+(defn find-stations-zipcode [zipcode mode]
+  (let [[lon lat] ((mc/find-one-as-map "zipcodes" {:_id (str zipcode)} {:_id 0, :loc 1}) :loc)]
+    (find-stations lat lon mode)))
+
+(defn find-stations-ip [ipaddr mode]
+  (let [{lat :latitude lon :longitude} (reports/get-geoip-data ipaddr)]
+    (find-stations lat lon mode)))
 
 ;(pprint (find-stations 37.62 -122.37 "metar"))
 ;(pprint (find-stations 37.62 -122.37 "metar"))
 
 ;(pprint (find-zipcode 63111))
+
+;(pprint (find-stations-zipcode 63111 "metar"))
+;(pprint (find-stations-ip "216.55.25.70" "metar"))
